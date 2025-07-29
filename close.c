3fe30c93478ba53a6c7f985cd7d9918cac5b1a82
Samuele Fiorini <sfiorini@c2r4p8.42firenze.it>
2025-07-29 10:29:00 +0200

    aggiunti i vari destroy image

--- close.c
+++ close.c
@@ -6,7 +6,7 @@
 /*   By: sfiorini <sfiorini@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/23 13:46:44 by sfiorini          #+#    #+#             */
-/*   Updated: 2025/07/26 10:55:47 by sfiorini         ###   ########.fr       */
+/*   Updated: 2025/07/29 10:28:29 by sfiorini         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -47,6 +47,11 @@ void	free_exec(t_exec *exec)
 	mlx_destroy_image(exec->mlx, exec->t.img_n);
 	mlx_destroy_image(exec->mlx, exec->t.img_s);
 	mlx_destroy_image(exec->mlx, exec->t.img_o);
+	mlx_destroy_image(exec->mlx, exec->t.img_o1);
+	mlx_destroy_image(exec->mlx, exec->t.img_o2);
+	mlx_destroy_image(exec->mlx, exec->t.img_shotgun);
+	mlx_destroy_image(exec->mlx, exec->t.img_shoot);
+	mlx_destroy_image(exec->mlx, exec->t.img_d);
 	mlx_destroy_image(exec->mlx, exec->t.img_e);
 	mlx_destroy_window(exec->mlx, exec->win);
 	mlx_destroy_display(exec->mlx);
